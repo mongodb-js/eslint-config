1a9830a994418c3677adc53d21e5f226fc4d4695
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-01 09:55:32 -0400

    allow process.exit

--- node.js
+++ node.js
@@ -7,6 +7,7 @@ module.exports = merge({}, base, {
   rules: {
     'handle-callback-err': [1, '^(err|.+Err|error|.+Error)$'],
     'no-path-concat': 2,
-    'no-sync': 1
+    'no-sync': 1,
+    'no-process-exit': 0
   }
 });
