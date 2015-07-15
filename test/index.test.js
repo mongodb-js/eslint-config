c6b1b868e4884b7b292a1cc9bcce7cbb289badfb
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-15 09:55:58 -0400

    fix broken test...

--- test/index.test.js
+++ test/index.test.js
@@ -1,33 +1,35 @@
-function isObject (obj) {
+function isObject(obj) {
   return typeof obj === 'object' && obj !== null;
 }
 
 var assert = require('assert');
 
-describe('eslint-config-mongodb-js', function () {
-  it('should have a base set of rules', function(){
+describe('eslint-config-mongodb-js', function() {
+  it('should have a base set of rules', function() {
     var config = require('../');
     assert(isObject(config.rules));
-    assert.equal(config.env, undefined);
+    assert.deepEqual(config.env, {
+      mocha: true
+    });
   });
-  it('should a config for node', function(){
+  it('should a config for node', function() {
     var config = require('../node');
     assert(isObject(config.rules));
     assert(isObject(config.env));
   });
-  it('should a config for the browser', function(){
+  it('should a config for the browser', function() {
     var config = require('../browser');
     assert(isObject(config.rules));
     assert(isObject(config.env));
   });
-  it('should a config for react', function(){
+  it('should a config for react', function() {
     var config = require('../react');
     assert(isObject(config.rules));
     assert(isObject(config.env));
     assert(isObject(config.ecmaFeatures));
     assert.equal(config.plugins[0], 'react');
   });
-  it('should a config for the mongo shell', function(){
+  it('should a config for the mongo shell', function() {
     var config = require('../shell');
     assert(config.env.mongodb);
   });
