794fc6b99fc7a551be03171acef445fa29af126d
Matt Fairbrass <matt.fairbrass@gmail.com>
2018-05-05 16:12:13 +1000

    fix: Remove ecmaFeatures block from rules as it is now deprecated and has no effect following upgrade to eslint@4.x.x (#45)

--- rules/ecmascript-6.js
+++ rules/ecmascript-6.js
@@ -13,24 +13,6 @@ module.exports = {
   env: {
     es6: false
   },
-  ecmaFeatures: {
-    arrowFunctions: true,
-    blockBindings: true,
-    classes: true,
-    defaultParams: true,
-    destructuring: true,
-    forOf: true,
-    generators: false,
-    modules: true,
-    objectLiteralComputedProperties: true,
-    objectLiteralDuplicateProperties: false,
-    objectLiteralShorthandMethods: true,
-    objectLiteralShorthandProperties: true,
-    spread: true,
-    superInFunctions: true,
-    templateStrings: true,
-    jsx: true
-  },
   rules: {
     // require parens in arrow function arguments
     'arrow-parens': IGNORE,
